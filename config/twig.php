1d2bed22bae2590c2ba6bb4a6bcfa55521f2716e
Nathalie CHARLES <89528903+NathalieCHA@users.noreply.github.com>
2021-10-20 18:25:58 +0200

    Add files via upload

---
+++ config/twig.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
+$twig = new Twig\Environment($loader, ['debug' => true]);
+$twig->addExtension(new Twig\Extension\DebugExtension());
+
+$email = 'bloblo@gmail.com';
+$twig->addGlobal('email', $email);
+
+?>
